e18e112c683e361d5f671f95eeecc583aa8ee53e
Jaehwa <shwoghk14@gmail.com>
2020-01-23 23:31:33 +0900

    Start 5 version 0.2

--- Start 5/test.py
+++ Start 5/test.py
@@ -1,8 +1,11 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
 kivy.require('1.11.1')
+
+class Upper_bar(BoxLayout):
+    pass
 
 class TestApp(App):
     pass
