8c8bef7da8b3fdf984b113bce19cc4cef55f631c
Jaehwa <shwoghk14@gmail.com>
2020-01-23 23:23:47 +0900

    Start 3 version 0.1

---
+++ Start 3/test.py
@@ -0,0 +1,12 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+kivy.require('1.11.1')
+
+class TestApp(App):
+    def build(self):
+        return Label(text='Hello World.')
+
+if __name__ == '__main__':
+    TestApp().run()
