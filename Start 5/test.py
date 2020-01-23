674905302dfc464287a0564adbcd5f40947b34b0
Jaehwa <shwoghk14@gmail.com>
2020-01-23 23:29:26 +0900

    Start 5 version 0.1

---
+++ Start 5/test.py
@@ -0,0 +1,11 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+kivy.require('1.11.1')
+
+class TestApp(App):
+    pass
+
+if __name__ == '__main__':
+    TestApp().run()
