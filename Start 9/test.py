08d494100cb4342381b6b4c026c337a09f0b6850
Jaehwa <shwoghk14@gmail.com>
2020-01-24 09:36:56 +0900

    Start 9 version 0.1

---
+++ Start 9/test.py
@@ -0,0 +1,17 @@
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+kivy.require('1.11.1')
+
+class ContentArea(BoxLayout):
+    pass
+
+class Upper_bar(BoxLayout):
+    pass
+
+class TestApp(App):
+    pass
+
+if __name__ == '__main__':
+    TestApp().run()
