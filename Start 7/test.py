37688cc3cf7453cae3906760fea763226da09925
Jaehwa <shwoghk14@gmail.com>
2020-01-24 09:20:47 +0900

    Start 7 version 0.1

---
+++ Start 7/test.py
@@ -0,0 +1,14 @@
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+kivy.require('1.11.1')
+
+class Upper_bar(BoxLayout):
+    pass
+
+class TestApp(App):
+    pass
+
+if __name__ == '__main__':
+    TestApp().run()
