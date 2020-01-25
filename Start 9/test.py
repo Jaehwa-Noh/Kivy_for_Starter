a2327fd24ee3fd437da5b8670d49a649a14634da
Jaehwa <shwoghk14@gmail.com>
2020-01-25 15:52:07 +0900

    Start 9 version 0.2

--- Start 9/test.py
+++ Start 9/test.py
@@ -1,14 +1,22 @@
 import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
 
 kivy.require('1.11.1')
+
+import os
 
 class ContentArea(BoxLayout):
     pass
 
 class Upper_bar(BoxLayout):
     pass
+
+class LocationPop(Popup):
+    def __init__(self, **kwargs):
+        super(LocationPop, self).__init__(**kwargs)
+        self.ids['filechooser'].path = os.getcwd()
 
 class TestApp(App):
     pass
